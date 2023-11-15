4302be15cb57ebd49c0cf6c73477c392870b7444
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2023-11-15 19:42:21 +0700

    update 1

---
+++ DeepFace/contrib/training/generate_train_labels.py
@@ -0,0 +1,15 @@
+# specify a directory where training images
+# are present, the directory should contain
+# images of each person as seperately
+# sub folders (name of each subfolder is a class label)
+
+TRAIN_PATH = 'D:/VGG-Face2/data/vggface2_train/train'
+
+import glob, os
+os.chdir(TRAIN_PATH)
+
+class_labels = glob.glob('*')
+
+with open('VGGFace2-class_labels_train.txt', 'w') as txt_file:
+     for cl in class_labels:
+          print(cl, file=txt_file)
